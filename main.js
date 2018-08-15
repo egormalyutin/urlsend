b362dee8e128f41a40fd348c7220502cf37f7477
Malyutin Egor <malyutinea@inbox.ru>
2018-08-15 13:31:38 +0600

    upd

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("views"))
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/data", function(req, res) {
-	res.send("succ")
-	console.log(req.query.location)
+app.use(function(req, res, next) {
+	console.log(req.url)
+	next()
 })
+
+app.use(express.static("views"))
 
 app.listen(PORT, function() {
 	console.log("Listening on http://localhost:" + PORT);
